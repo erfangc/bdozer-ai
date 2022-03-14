88a4336b1336c112e436893f7228406a29d81978
Erfang Chen <erfangc@gmail.com>
2022-03-14 12:11:29 -0400

    added some more code

---
+++ core-nlp/download_pretrained_models.py
@@ -0,0 +1,22 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+cross_encoder_model = 'cross-encoder/ms-marco-MiniLM-L-6-v2'
+
+
+def save_pretrained_model(model_name: str):
+    """
+    Downloads a pretrained model and save them to local filesystem on the project path
+    so they do not have to be re-downloaded, furthermore they can be added to Docker containers or 
+    VM runtimes for a hosted API solution
+    :param model_name: the HuggingFace pretrained model name
+    :return: nothing
+    """
+    print(f"Saving pre-trained model {model_name} to local ./{model_name}")
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    model.save_pretrained(f'./{model_name}')
+    tokenizer.save_pretrained(f'./{model_name}')
+
+
+if __name__ == "__main__":
+    save_pretrained_model(cross_encoder_model)
--- core-nlp/shared_objects.py
+++ core-nlp/shared_objects.py
@@ -2,7 +2,9 @@ import spacy
 from fastapi import FastAPI
 from sentence_transformers import CrossEncoder
 
+from download_pretrained_models import cross_encoder_model
+
 app = FastAPI()
 
 nlp = spacy.load("en_core_web_trf")
-cross_encoder = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-6-v2')
+cross_encoder = CrossEncoder(cross_encoder_model)
