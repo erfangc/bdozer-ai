3b7704d79d9536edab46161ae4a95727cb047018
Erfang Chen <erfangc@gmail.com>
2022-04-16 17:06:33 -0400

    wip

--- ten-k-parser/src/main/kotlin/co/bdozer/Driver.kt
+++ ten-k-parser/src/main/kotlin/co/bdozer/Driver.kt
@@ -1,6 +1,7 @@
 package co.bdozer
 
 import co.bdozer.models.CompanyTicker
+import co.bdozer.models.Recent
 import co.bdozer.models.Submission
 import co.bdozer.sectionparser.Section
 import co.bdozer.sectionparser.SectionParser
@@ -9,6 +10,12 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import okio.utf8Size
+import org.apache.http.HttpHost
+import org.elasticsearch.action.index.IndexRequest
+import org.elasticsearch.client.RequestOptions
+import org.elasticsearch.client.RestClient
+import org.elasticsearch.client.RestHighLevelClient
+import org.elasticsearch.common.xcontent.XContentType
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
@@ -20,39 +27,76 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpRequest.BodyPublishers
 import java.net.http.HttpResponse.BodyHandlers
-import java.util.Stack
+import java.security.MessageDigest
+import java.time.Instant
+import java.util.*
+import kotlin.collections.ArrayDeque
+import kotlin.collections.List
+import kotlin.collections.Map
+import kotlin.collections.arrayListOf
+import kotlin.collections.emptyList
+import kotlin.collections.find
+import kotlin.collections.forEach
+import kotlin.collections.indexOfFirst
+import kotlin.collections.indices
+import kotlin.collections.isNotEmpty
+import kotlin.collections.joinToString
+import kotlin.system.exitProcess
 
 private val log: Logger = LoggerFactory.getLogger("Driver")
 
 private val objectMapper =
-    jacksonObjectMapper()
-        .findAndRegisterModules()
-        .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true)
+    jacksonObjectMapper().findAndRegisterModules().configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true)
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
+private val restHighlevelClient = RestHighLevelClient(RestClient.builder(HttpHost("localhost", 9200)))
+
 fun main() {
+    val tickers = FileInputStream("ten-k-parser/tickers.txt")
+        .bufferedReader()
+        .readLines()
+        .map { it.trim() }
+        .filter { it.isNotBlank() }
+
     val start = System.currentTimeMillis()
     val tickerMap =
         objectMapper.readValue<Map<String, CompanyTicker>>(FileInputStream("ten-k-parser/company_tickers.json"))
     val end = System.currentTimeMillis()
-
-    val ticker = "ADBE"
     log.info("Loading company tickers took ${end - start}ms")
+
+    for (ticker in tickers) {
+        try {
+            processSingleCompany(tickerMap, ticker)
+        } catch (e: Exception) {
+            log.error("Exception occurred while processing ticker={}, error={}", ticker, e.message)
+        }
+    }
+    exitProcess(0)
+}
+
+private fun processSingleCompany(
+    tickerMap: Map<String, CompanyTicker>, ticker: String
+) {
+
     val company = tickerMap.entries.find { it.value.ticker == ticker }?.value ?: error("...")
     val cik = company.cik_str.padStart(length = 10, padChar = '0')
 
     // ------------------------------------------------------
     // Find the latest submission and print out the raw text
     // ------------------------------------------------------
-    val submission = submission(cik)
-    val idx = submission.filings?.recent?.form?.indexOfFirst { it == "10-K" } ?: error("...")
+    val submission = getSubmission(cik)
 
-    val ash = submission.filings.recent.accessionNumber?.get(idx) ?: error("...")
-    val primaryDocument = submission.filings.recent.primaryDocument?.get(idx) ?: error("...")
-    val latest10K = "https://www.sec.gov/Archives/edgar/data/$cik/${ash.replace("-", "")}/$primaryDocument"
-    log.info("cik=$cik ash=$ash primaryDocument=$primaryDocument latest10K=${latest10K}")
+    val form = "10-K"
+    val idx = submission.filings?.recent?.form?.indexOfFirst { it == form } ?: error("...")
+    val recent = submission.filings.recent
+    val ash = recent.accessionNumber?.get(idx) ?: error("...")
+    val reportDate = recent.reportDate?.get(idx) ?: error("...")
 
-    val doc = Jsoup.connect(latest10K).get()
+    val primaryDocument = recent.primaryDocument?.get(idx) ?: error("...")
+    val url = "https://www.sec.gov/Archives/edgar/data/$cik/${ash.replace("-", "")}/$primaryDocument"
+    log.info("Parsing form=$form cik=$cik ash=$ash primaryDocument=$primaryDocument latest10K=${url}")
+
+    val doc = Jsoup.connect(url).get()
     val sectionParser = SectionParser()
     val sections = sectionParser.findSections(doc)
     val elements = filterDownToSection(doc, sections.business)
@@ -61,19 +105,53 @@ fun main() {
 
     log.info("bodySize={}", body.toString().utf8Size())
     val textBody = HttpClient.newHttpClient().send(
-        HttpRequest
-            .newBuilder()
-            .POST(BodyPublishers.ofString(body.toString()))
-            .header("Content-Type", "text/plain")
-            .uri(URI.create("http://localhost:3000/convert"))
-            .build(),
+        HttpRequest.newBuilder().POST(BodyPublishers.ofString(body.toString())).header("Content-Type", "text/plain")
+            .uri(URI.create("http://localhost:3000/convert")).build(),
         BodyHandlers.ofString(),
     ).body()
-    
-    println(textBody)
+
+    // ------------------------------
+    // Put the extracted data into ES
+    // ------------------------------
+    val section = "Business"
+    val esFiling = ESFiling(
+        cik = cik,
+        ash = ash,
+        url = url,
+        reportDate = reportDate,
+        text = textBody,
+        section = section,
+        form = form,
+        ticker = ticker,
+        companyName = submission.name ?: "Unknown",
+        timestamp = Instant.now().toString(),
+    )
+    val indexResponse = restHighlevelClient.index(
+        IndexRequest("filings").source(objectMapper.writeValueAsString(esFiling), XContentType.JSON),
+        RequestOptions.DEFAULT,
+    )
+    log.info("Indexed document, result={}, ticker={}", indexResponse.result, ticker)
 }
 
-private fun submission(cik: String): Submission {
+val digest: MessageDigest = MessageDigest.getInstance("SHA-256")
+fun hash(vararg components: String): String {
+    val str = components.joinToString()
+    return bytesToHex(digest.digest(str.encodeToByteArray()))
+}
+
+fun bytesToHex(hash: ByteArray): String {
+    val hexString = StringBuilder(2 * hash.size)
+    for (i in hash.indices) {
+        val hex = Integer.toHexString(0xff and hash[i].toInt())
+        if (hex.length == 1) {
+            hexString.append('0')
+        }
+        hexString.append(hex)
+    }
+    return hexString.toString()
+}
+
+private fun getSubmission(cik: String): Submission {
     log.info("CIK=$cik")
     val inputStream = HttpClient.newHttpClient().send(
         HttpRequest.newBuilder().GET().uri(URI.create("https://data.sec.gov/submissions/CIK${cik}.json")).build(),
---
+++ ten-k-parser/src/main/kotlin/co/bdozer/ESFiling.kt
@@ -0,0 +1,14 @@
+package co.bdozer
+
+data class ESFiling(
+    val cik: String,
+    val ash: String,
+    val url: String,
+    val reportDate: String,
+    val text: String,
+    val section: String,
+    val form: String,
+    val ticker: String,
+    val companyName: String,
+    val timestamp: String,
+)
