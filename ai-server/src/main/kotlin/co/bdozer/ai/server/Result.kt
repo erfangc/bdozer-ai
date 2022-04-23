f6f8ca64880a250e05a185b22d211e49327ae211
Erfang Chen <erfangc@gmail.com>
2022-04-22 21:15:22 -0400

    updated Result.kt

--- ai-server/src/main/kotlin/co/bdozer/ai/server/Result.kt
+++ ai-server/src/main/kotlin/co/bdozer/ai/server/Result.kt
@@ -1,3 +1,8 @@
 package co.bdozer.ai.server
 
-data class Result(val cik: String? = null, val score: Double? = null, val ticker: String? = null, val companyName:String? =null)
+data class Result(
+    val cik: String? = null,
+    val score: Double? = null,
+    val ticker: String? = null,
+    val companyName: String? = null
+)
