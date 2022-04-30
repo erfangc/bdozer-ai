a1213ef3171b014ba199660d1104e5fabd2e93f2
Erfang Chen <erfangc@gmail.com>
2022-04-30 19:32:38 -0400

    wip

--- crawler/src/main/kotlin/co/bdozer/master/CompanyMasterDriver.kt
+++ crawler/src/main/kotlin/co/bdozer/master/CompanyMasterDriver.kt
@@ -1,9 +1,21 @@
 package co.bdozer.master
 
-import co.bdozer.utils.Beans
-
-private val restHighLevelClient = Beans.restHighLevelClient()
+import co.bdozer.utils.DataAccess.query
 
 fun main() {
-    
+    val results = query(
+        sql = """
+        select 
+            m_ticker,
+            per_end_date,
+            diluted_net_eps
+        from
+            fc
+        where
+            m_ticker = 'TDOC' AND per_type = 'A'
+        order by 
+            per_end_date desc
+    """.trimIndent()
+    )
+    println(results.toList())
 }
